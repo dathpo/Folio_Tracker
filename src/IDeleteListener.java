366215ed38f83b7728910b0727e9cc39ec2d1501
David Pocock <dsb13181@ds.strath.ac.uk>
2016-11-24 16:59:13 +0000

    Deleted old source files

--- src/IDeleteListener.java
+++
@@ -1,9 +0,0 @@
-import java.util.Set;
-
-public interface IDeleteListener {
-  
-   public Set<Action> getAction();
-
-   public Set<Menu> getMenu();
-   
-   }
--- src/IObserver.java
+++
@@ -1,7 +0,0 @@
-import java.util.Set;
-
-public interface IObserver {
-
-   public Set<ITracker> getITracker();
-   
-   }
